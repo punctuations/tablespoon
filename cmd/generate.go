658e8b4256e19d1f35950302107462316cd4395d
matt <mattsnaccc@gmail.com>
2021-11-25 13:34:41 -0800

    feat(init): generate command

---
+++ cmd/generate.go
@@ -0,0 +1,22 @@
+package cmd
+
+import (
+	"github.com/pterm/pterm"
+	"github.com/spf13/cobra"
+	"time"
+)
+
+// dateCmd represents the date command
+var generateCmd = &cobra.Command{
+	Use:   "generate",
+	Short: "Generates a commit message.",
+	Run: func(cmd *cobra.Command, args []string) {
+		format, _ := cmd.Flags().GetString("format")
+		letters := pterm.NewLettersFromString(time.Now().Format(format))
+		pterm.DefaultBigText.WithLetters(letters).Render()
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(generateCmd)
+}
