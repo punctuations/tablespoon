5d1b1628f5985aabcb415d1051a2fde9a16b111b
matt <mattsnaccc@gmail.com>
2021-11-29 13:27:43 -0800

    fix?: fix commit command?

--- cmd/commit.go
+++ cmd/commit.go
@@ -13,7 +13,7 @@ var commitCmd = &cobra.Command{
 	Short: "Generates a commit message & commits it.",
 	Run: func(cmd *cobra.Command, args []string) {
 		input := "test: test!!"
-		out, err := exec.Command(fmt.Sprintf("git commit -m '%s'", input)).Output()
+		out, err := exec.Command(fmt.Sprintf("git commit -m '%s'", input), "").Output()
 
 		// if there is an error with our execution handle it here
 		if err != nil {
