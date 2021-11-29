238f59122b70c558280ce49b40f3a3466c7008a0
matt <mattsnaccc@gmail.com>
2021-11-29 14:12:15 -0800

    feat: add *some* functionality to generate command

--- cmd/generate.go
+++ cmd/generate.go
@@ -3,7 +3,8 @@ package cmd
 import (
 	"github.com/pterm/pterm"
 	"github.com/spf13/cobra"
-	"time"
+	"io/ioutil"
+	"os/exec"
 )
 
 // generateCmd represents the generate command
@@ -11,9 +12,17 @@ var generateCmd = &cobra.Command{
 	Use:   "generate",
 	Short: "Generates a commit message.",
 	Run: func(cmd *cobra.Command, args []string) {
-		format, _ := cmd.Flags().GetString("format")
-		letters := pterm.NewLettersFromString(time.Now().Format(format))
-		pterm.DefaultBigText.WithLetters(letters).Render()
+		exec.Command("git", "diff", "--numstat", "--output='./tmp/log.txt'")
+
+		content, err := ioutil.ReadFile("text.txt")
+		if err != nil {
+			pterm.Error.Println(err)
+		}
+
+		pterm.Success.Println(string(content))
+		//format, _ := cmd.Flags().GetString("format")
+		//letters := pterm.NewLettersFromString(time.Now().Format(format))
+		//pterm.DefaultBigText.WithLetters(letters).Render()
 	},
 }
 
