1197468446d95058bc3e52d93fd94cfd892754a3
matt <mattsnaccc@gmail.com>
2021-12-07 13:23:24 -0800

    chore: code cleanup

--- cmd/generate.go
+++ cmd/generate.go
@@ -20,97 +20,82 @@ var generateCmd = &cobra.Command{
 		}
 
 		out := strings.Fields(string(content))
+		message, file, verb, adds, dels := rules(out)
 
-		var diffs []int
-		var files []string
-		var adds int
-		var dels int
+		pterm.Success.Printf("%s(%s): %s", message, file, verb)
 
-		for i := range out {
-			deletions, _ := strconv.Atoi(out[i])
-			pattern := regexp.MustCompile("0|[1-9][0-9]*")
-			if (i+1)%3 != 0 {
-				if i == 0 { // initialize all the additions + deletions and store
-					del, _ := strconv.Atoi(out[i+1])
-					adds = deletions // not actually deletions in this case
-					dels = del
-				}
-				if i >= 1 {
-					if pattern.MatchString(out[i-1]) {
-						additions, _ := strconv.Atoi(out[i-1])
-						changes := deletions + additions
-						diffs = append(diffs, []int{changes}...)
-						files = append(files, []string{out[i+1]}...)
-						if additions+deletions > adds+dels {
-							adds = additions
-							dels = deletions
-						}
-					}
-				}
-				//fmt.Println(outInt)
-			}
-		}
-
-		selected := []string{""} // initializing value
-
-		for n := range diffs {
-			s, _ := strconv.Atoi(selected[0])
-			if n == 0 {
-				selected = []string{strconv.Itoa(diffs[n]), files[n]}
-			} else if diffs[n] > s {
-				selected = []string{strconv.Itoa(diffs[n]), files[n]}
-			}
-		}
-
-		var message string
-		var verb string
-
-		switch {
-		case adds-dels == 0:
-			message = "fix"
-			verb = "fix"
-		case adds-dels <= 5:
-			message = "fix"
-			verb = "fix"
-		case adds-dels >= -5:
-			message = "fix"
-			verb = "fix"
-		case adds-dels > 5:
-			message = "feat"
-			verb = "add"
-		case adds-dels < -5:
-			message = "feat"
-			verb = "remove"
-
-		default:
-			message = "feat"
-			verb = "add"
-		}
-
-		if adds-dels == 0 {
-			message = "fix"
-			verb = "fix"
-		} else if adds-dels <= 5 {
-			message = "fix"
-			verb = "fix"
-		} else if adds-dels >= -5 {
-			message = "fix"
-			verb = "fix"
-		} else if adds-dels > 5 {
-			message = "feat"
-			verb = "add"
-		} else if adds-dels < -5 {
-			message = "feat"
-			verb = "remove"
-		}
-
-		pterm.Success.Printf("%s(%s): %s", message, selected[1], verb)
-
-		pterm.Success.Println(selected, " + ", adds, " - ", dels)
+		pterm.Success.Println(file, " + ", adds, " - ", dels)
 		//format, _ := cmd.Flags().GetString("format")
 		//letters := pterm.NewLettersFromString(time.Now().Format(format))
 		//pterm.DefaultBigText.WithLetters(letters).Render()
 	},
+}
+
+func rules(input []string) (message string, file string, verb string, adds int, dels int) {
+	var diffs []int
+	var files []string
+
+	for i := range input {
+		deletions, _ := strconv.Atoi(input[i])
+		pattern := regexp.MustCompile("0|[1-9][0-9]*")
+		if (i+1)%3 != 0 {
+			if i == 0 { // initialize all the additions + deletions and store
+				del, _ := strconv.Atoi(input[i+1])
+				adds = deletions // not actually deletions in this case
+				dels = del
+			}
+			if i >= 1 {
+				if pattern.MatchString(input[i-1]) {
+					additions, _ := strconv.Atoi(input[i-1])
+					changes := deletions + additions
+					diffs = append(diffs, []int{changes}...)
+					files = append(files, []string{input[i+1]}...)
+					if additions+deletions > adds+dels {
+						adds = additions
+						dels = deletions
+					}
+				}
+			}
+			//fmt.Println(outInt)
+		}
+	}
+
+	selected := []string{""} // initializing value
+
+	for n := range diffs {
+		s, _ := strconv.Atoi(selected[0])
+		if n == 0 {
+			selected = []string{strconv.Itoa(diffs[n]), files[n]}
+		} else if diffs[n] > s {
+			selected = []string{strconv.Itoa(diffs[n]), files[n]}
+		}
+	}
+
+	switch {
+	case adds-dels == 0:
+		message = "fix"
+		verb = "fix"
+	case adds-dels <= 5:
+		message = "fix"
+		verb = "fix"
+	case adds-dels >= -5:
+		message = "fix"
+		verb = "fix"
+	case adds-dels > 5:
+		message = "feat"
+		verb = "add"
+	case adds-dels < -5:
+		message = "feat"
+		verb = "remove"
+
+	default:
+		message = "feat"
+		verb = "add"
+	}
+
+	file = selected[1]
+
+	return
 }
 
 func init() {
