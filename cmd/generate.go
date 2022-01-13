f90d071215e36f1a8ab51cca8acc88d7e3fd3d2a
matt <matt@dont-ping.me>
2022-01-13 14:09:09 -0800

    fix: remove unused code

--- cmd/generate.go
+++ cmd/generate.go
@@ -262,9 +262,9 @@ func rules(input []string, unstaged bool, ncomment bool, selectFlag string) (mes
 				println(strings.Split(string(wdiff), "+"))
 				if len(strings.Split(string(newEntry[i]), commentID)) > 0 {
 					// prints //!#balls not "//" or "balls" like it should. wrap in another split if all else fails???
-					println(strings.Split(strings.Split(strings.Split(string(newEntry[i]), commentID)[0], "\n")[0], commentID)[0])
+					println(strings.Split(strings.Split(string(newEntry[i]), commentID)[0], "\n")[0])
 
-					short = strings.Split(strings.Split(strings.Split(string(newEntry[i]), commentID)[0], "\n")[0], commentID)[0]
+					short = strings.Split(strings.Split(string(newEntry[i]), commentID)[0], "\n")[0]
 				}
 			}
 		}
