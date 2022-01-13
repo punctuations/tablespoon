88930f42bd2dde612e4d03fb865be50766d5a270
matt <matt@dont-ping.me>
2022-01-12 16:09:12 -0800

    test: debug

--- cmd/generate.go
+++ cmd/generate.go
@@ -239,7 +239,7 @@ func rules(input []string, unstaged bool, ncomment bool, selectFlag string) (mes
 
 		short = shortened
 	} else {
-		println(strings.Split(string(wdiff), commentID), len(strings.Split(string(wdiff), commentID)))
+		println(strings.Split(string(wdiff), commentID)[0], len(strings.Split(string(wdiff), commentID)))
 		if len(strings.Split(string(wdiff), commentID)) < 2 {
 			userShort := promptui.Prompt{
 				Label:   fmt.Sprintf("What was changed in %s?", file),
