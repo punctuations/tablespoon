59f14c2a6d254ddc7e4c89ab4b947df11065c7b0
matt <matt@dont-ping.me>
2022-03-19 11:52:52 -0700

    chore(update): update without sudo

--- cmd/update.go
+++ cmd/update.go
@@ -3,10 +3,8 @@ package cmd
 import (
 	"github.com/pterm/pterm"
 	"github.com/spf13/cobra"
-	"os"
 	"os/exec"
 	"runtime"
-	"strings"
 )
 
 // updateCmd represents the update command
@@ -14,28 +12,15 @@ var updateCmd = &cobra.Command{
 	Use:   "update",
 	Short: "Update the cli tool",
 	Run: func(cmd *cobra.Command, args []string) {
-		shellPath := os.Getenv("SHELL")
 
 		os := runtime.GOOS
 		switch os {
 		case "windows": // self-explanatory
 			exec.Command("iwr instl.sh/punctuations/tablespoon/windows | iex")
 		case "darwin": // macOS
-			shell := strings.Split(shellPath, "/")[len(strings.Split(shellPath, "/"))-1]
-
-			if shell == "bash" || shell == "sh" {
-				exec.Command("curl -sSL instl.sh/punctuations/tablespoon/macos | sudo bash")
-			} else {
-				exec.Command("sudo curl -sSL instl.sh/punctuations/tablespoon/macos | sudo bash")
-			}
+			exec.Command("curl -sSL instl.sh/punctuations/tablespoon/macos | bash")
 		default: // everything else basically
-			shell := strings.Split(shellPath, "/")[len(strings.Split(shellPath, "/"))-1]
-
-			if shell == "bash" || shell == "sh" {
-				exec.Command("curl -sSL instl.sh/punctuations/tablespoon/linux | sudo bash")
-			} else {
-				exec.Command("sudo curl -sSL instl.sh/punctuations/tablespoon/linux | sudo bash")
-			}
+			exec.Command("curl -sSL instl.sh/punctuations/tablespoon/linux | bash")
 		}
 
 		pterm.Success.Println("✨ Command Successfully Executed")
